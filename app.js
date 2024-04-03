290676f3a40561618602e45c6339ec2755b8ae33
Whoami <nurlanguliev33@gmail.com>
2024-04-03 05:47:16 -0700

    javascr5

---
+++ app.js
@@ -0,0 +1,113 @@
+// TASK-1 
+// let products = [
+//       {
+//         id: 1,
+//         name: "Redmi note 10 pro",
+//         price: 500,
+//       },
+//       {
+//         id: 2,
+//         name: "Kia Cerato",
+//         price: 20000,
+//       },
+//       {
+//         id: 3,
+//         name: "Samsung A70",
+//         price: 600,
+//       },
+//       {
+//         id: 4,
+//         name: "Iphone", 
+//         price: 3500,
+//       },
+//     ];
+//     let tapaq = [];
+//     products.sort((a, b) => a.price - b.price);
+//     products.forEach((product) => {
+//       console.log(`${product.name}: ${product.price}`);
+//     });
+
+
+
+// TASK-2
+
+// let numbers = [];
+// for (let i = 0; i<5; i++){
+//     numbers.push(Math.floor(Math.random()*100)+1);
+// }
+// let sum = 0;
+// for(let j =0; j<numbers.length; j++){
+//     sum+= numbers[j];
+// }
+// console.log(numbers);
+// console.log(sum);
+
+
+// TASK-3
+// Js forEach() fonksiyonu ile bir dizideki tüm elemanlara kolaylıkla dolaşabiliriz.
+
+// Js forEach() fonksiyonu Array nesnesi yani bir dizi üzerinden çağrılmalıdır.
+
+// Js forEach() fonksiyonuna gönderdiğimiz callback fonksiyonu dizideki her bir elemana ulaşıldığında çağrılır.
+
+// Örnek:
+
+// const sayilar = [1, 2, 3];
+
+// sayilar.forEach(myFunction);
+
+// function myFunction(sayi) {
+//   console.log(sayi);
+// }
+
+// 1
+// 2
+// 3
+// Sayilar dizisi üzerindeki her bir elemana ulaşıldığında myFunction fonksiyonu çağrılır ve fonksiyona gelen dizi elemanı ekrana yazdırılır.
+
+// Her bir dizi elemanı için çağrılan fonksiyona callback fonksiyonu denir.
+
+
+
+
+
+// TASK-4
+// 1.  Kullanıcı tarafından oluşturulan verilerden JSON nesnesi oluşturma
+// JSON, geçici verileri depolamak için idealdir. Örneğin, geçici veriler bir web sitesinde gönderilen form gibi kullanıcı tarafından oluşturulmuş veriler olabilir. JSON, yüksek düzeyde birlikte çalışabilirlik sunmak için tüm programlama dillerine yönelik bir serileştirme veri formatı olarak da kullanılabilir.
+
+// 2.  Verileri sistemler arasında aktarma
+// Bir web sitesi veritabanında müşterinin posta adresi bulunmaktadır ancak geçerli olduğundan emin olmak için adresin bir API üzerinden doğrulanması gerekir. Adres verilerini JSON formatında adres doğrulama hizmeti API'sine gönderin.
+
+// 3.  Verileri uygulamalar için konfigüre etme
+// Uygulama geliştirirken her uygulamanın bir veritabanına ve günlük dosyası dizin yoluna bağlanmak için kimlik bilgilerine ihtiyacı olur. Kimlik bilgileri ve günlük dosyası yolu, okunacak ve kullanılabilir olacak bir JSON dosyasında belirtilebilir.
+
+// 4.  Karmaşık veri modellerini basitleştirme
+// JSON, veri çıkartma sürecini öngörülebilir ve insanlar tarafından okunabilir bir JSON dosyasına dönüştürerek karmaşık dokümanları anlamlı olarak tanımlanan bileşenler şeklinde basitleştirir.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
